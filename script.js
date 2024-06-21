a5db09b7b457bbf00e8a54803ff52861f3e907b9
Carlos044(Muertito) <carlossayid044@gmail.com>
2024-06-21 04:16:37 -0600

    Adición de ciertos detalles y prueba de script para safari en efecto blur

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open');
     const isOpen = dropDownMenu.classList.contains('open');
     toggleBtnIcon.classList = isOpen
-    ? 'bi bi-x-lg'
-    : 'bi bi-list'
+        ? 'bi bi-x-lg'
+        : 'bi bi-list'
 }
+
+document.getElementById('mail').addEventListener('click', function (event) {
+    event.preventDefault();
+    const email = "rocio.sandoval.chavez@cbtis118.edu.mx";
+    navigator.clipboard.writeText(email).then(function () {
+        alert('Correo electrónico copiado al portapapeles');
+    }).catch(function (err) {
+        console.error('Error al copiar al portapapeles: ', err);
+    });
+});
+
+// Script para detectar si soporta el efecto borroso del mini menu 
+// Si no lo soporta lo cambia por un fondo semi transparente
+document.addEventListener('DOMContentLoaded', function() {
+    if (!CSS.supports('backdrop-filter', 'blur(10px)')) {
+        document.body.classList.add('no-navbar');
+    }
+});
